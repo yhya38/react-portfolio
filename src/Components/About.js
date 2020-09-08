a7c12efd08634b0005538cbf5a6cbc56bcba7302
yk2217 <yahiyakhan2217@gmail.com>
2020-09-08 17:09:10 +0530

    react portfolio

---
+++ src/Components/About.js
@@ -0,0 +1,66 @@
+// eslint-disable-next-line
+
+import React, { Component } from "react";
+
+class About extends Component {
+  render() {
+    if (this.props.data) {
+      var name = this.props.data.name;
+      var profilepic = "images/" + this.props.data.image;
+      var bio = this.props.data.bio;
+      var street = this.props.data.address.street;
+      var city = this.props.data.address.city;
+      var state = this.props.data.address.state;
+      var zip = this.props.data.address.zip;
+      var phone = this.props.data.phone;
+      var email = this.props.data.email;
+      var resumeDownload = this.props.data.resumedownload;
+    }
+
+    return (
+      <section id="about">
+        <div className="row">
+          <div className="three columns">
+            <img
+              className="profile-pic"
+              src="https://instagram.fblr4-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.118.1080.1080a/s640x640/75572958_516037679233464_5047775551675517236_n.jpg?_nc_ht=instagram.fblr4-2.fna.fbcdn.net&amp;_nc_cat=100&amp;_nc_ohc=B_aBKvxZptYAX90AE9z&amp;oh=531b2ea27f545063fecaf482cea3c1b2&amp;oe=5F6BA298"
+              alt="Yahiya Profile Pic"
+            />
+          </div>
+          <div className="nine columns main-col">
+            <h2>About Me</h2>
+
+            <p>{bio}</p>
+            <div className="row">
+              <div className="columns contact-details">
+                <h2>Contact Details</h2>
+                <p className="address">
+                  <span>{name}</span>
+                  <br />
+                  <span>
+                    {street}
+                    <br />
+                    {city} {state}, {zip}
+                  </span>
+                  <br />
+                  <span>{phone}</span>
+                  <br />
+                  <span>{email}</span>
+                </p>
+              </div>
+              <div className="columns download">
+                <p>
+                  <a href={resumeDownload} className="button">
+                    <i className="fa fa-download"></i>Download Resume
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default About;
